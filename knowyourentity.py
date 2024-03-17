1bc48052fb016a6c8a787e5f8a8f61132cccc28e
Enrico Renna <ann3r@pm.me>
2024-03-17 13:08:08 +0000

    fix(linting): improved code formatting to match PEP8 standards

--- knowyourentity.py
+++ knowyourentity.py
@@ -1,3 +1,8 @@
+"""
+Know Your Entity main script.
+Handles arguments, logging and input validation
+"""
+
 import argparse
 import ipaddress
 import logging
@@ -5,65 +10,93 @@ import sys
 
 logger = logging.getLogger(__name__)
 
+
 def parse_args():
+    """Parses the arguments provided via CLI."""
+
     # Define the argument parser
     parser = argparse.ArgumentParser(
-        prog='knowyourentity.py',
-        description='An OSINT federated information retrieval framework.',
-        epilog='Developed by Enrico Renna. Licenced under GPLv3.0.'
+        prog="knowyourentity.py",
+        description="An OSINT federated information retrieval framework.",
+        epilog="Developed by Enrico Renna. Licenced under GPLv3.0.",
     )
 
     # Provide the viable arguments
-    parser.add_argument('entity', 
-                        type=str, 
-                        help='The entity to be analysed. Currently only IP addresses are supported.')
-    parser.add_argument('-v', '--verbose', 
-                        type=str, 
-                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                        default='INFO', 
-                        help='The level of verbosity of the script. Default and recommended is INFO.')
+    parser.add_argument(
+        "entity",
+        type=str,
+        help="The entity to be analysed. Currently only IP addresses are supported.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        type=str,
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="INFO",
+        help="The level of verbosity of the script. Default and recommended is INFO.",
+    )
 
     # Parse and return arguments
     return parser.parse_args()
 
+
 def log(args):
+    """Configure logging for the framework"""
+
     # Setup logging variables
-    log_file = './logs/' + args.entity + '.log'
+    log_file = "./logs/" + args.entity + ".log"
 
     # Configure logger
-    logging.basicConfig(filename=log_file,
-                        encoding='utf-8',
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        level=args.verbose)
-    
+    logging.basicConfig(
+        filename=log_file,
+        encoding="utf-8",
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=args.verbose,
+    )
+
     # Also log to stdout
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-    
-    # Begin logging
-    logger.info('Know Your Entity has started.')
-    logger.info('The entity to be investigated is ' + args.entity + '.')
-    logger.info('The verbosity has been set to ' + args.verbose + '.')
 
-# Verify whether the input is a valid IP address
+    # Begin logging
+    logger.info("Know Your Entity has started.")
+    logger.info("The entity to be investigated is %s.", args.entity)
+    logger.info("The verbosity has been set to %s.", args.verbose)
+
+
 def validate_input(entity):
+    """Verify whether the input is a valid IP address"""
+
     try:
         address = ipaddress.ip_address(entity)
         if address.is_private:
-            logger.warning('The IPv' + str(address.version) + ' address ' + entity + ' is a private address reserved for internal network use, not accessible from the internet, and typically used for communication within a local network.')
+            logger.warning(
+                "The IPv%s address %s is a private address reserved for internal network use,"\
+                "not accessible from the internet, and typically used for communication within"\
+                " a local network.",
+                str(address.version),
+                entity,
+            )
             done()
-    except (ValueError) as e:
-        logger.critical('The entity ' + str(e) + '. The program will now exit.')
+    except ValueError as e:
+        logger.critical("The entity %s. The program will now exit.", str(e))
+
 
 def done():
-    logger.info('All operations have been completed. Exiting...')
+    """Nothing else to do, exit."""
+
+    logger.info("All operations have been completed. Exiting...")
     exit(1)
 
+
 def main():
-    args = parse_args() # Retrieve arguments
+    """Main function, calls other functions."""
+
+    args = parse_args()  # Retrieve arguments
     log(args)
     validate_input(args.entity)
     done()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
